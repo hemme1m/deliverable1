6a38e8cd7db46e8c86e44be9e8daf6c7c687d0f4
hemme1m <markhemmert@gmail.com>
2017-06-29 14:08:17 -0400

    js fix attempt
    switching from alert to div on page that prints out result

--- HemmertPortfolio/contact/funform.js
+++ HemmertPortfolio/contact/funform.js
@@ -1,14 +1,17 @@
 // handles the user's input in the name field
 function getUserInfo() {
 	var funUsername = document.getElementById("usersName").value;
+	var result = document.getElementById("result");
 //alert user that name has to be at least two characters if 0 or 1 character entered	
 	
 	if (funUsername.length < 2) {
-		alert("Your name must contain at least 2 characters.");
+		result.textContent = ("Your name must contain at least 2 characters.");
+//		alert("Your name must contain at least 2 characters.");
  // return the user's input in different ways
 	} else {
     	var bigName = funUsername.toUpperCase();
-		alert("Wow, " + funUsername + "! I just want to shout your name--" + bigName + "! " + bigName + "! Sorry, I got excited.");
+    	result.textContent = ("Wow, " + funUsername + "! I just want to shout your name--" + bigName + "! " + bigName + "! Sorry, I got excited.");
+//		alert("Wow, " + funUsername + "! I just want to shout your name--" + bigName + "! " + bigName + "! Sorry, I got excited.");
 	}
 }
 // when the user clicks the button run the getUserInfo function
